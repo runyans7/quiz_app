a396f21e6f86ed0d50c3e57c426d949f9a9fd541
Scotty Runyan <runyans7@gmail.com>
2014-10-31 10:10:51 -0400

    User can answer questions. Quiz keeps track of question number

--- assets/js/application.js
+++ assets/js/application.js
@@ -16,69 +16,106 @@ $(document).ready(function() {
   var question1 = new Question(
     "Memorial Stadium, also known as Death Valley, is home to the Tigers from which university?",
     ["LSU", "Clemson", "UVA", "Mississippi"],
-    1
+    "Clemson"
   );
 
   var question2 = new Question(
     "With two of the most dramatic wins in NCAA history, the team won the SEC Championship in 2013.",
     ["Georgia", "Alabama", "Auburn", "Texas Tech"],
-    2
+    "Auburn"
   );
 
   var question3 = new Question(
     "Known as the world's largest cocktail party, the Georgia Bulldog's take on this team in Jacksonville.",
     ["Florida State", "Florida", "Auburn", "USC"],
-    1  
+    "Florida"  
   );
 
   var question4 = new Question(
     "This historic team, will begin playing an ACC heavy schedule in 2015.",
     ["Alabama", "Penn State", "Notre Dame", "Southern Cal"],
-    2
+    "Notre Dame"
   );
 
   var question5 = new Question(
     "This team has the most ACC conference championships.",
     ["Florida State", "Georgia Tech", "Miami", "Clemson"],
-    3
+    "Clemson"
   );
 
   /*--- Stores questions in an array ---*/
   questionBank = [question1, question2, question3, question4, question5];
 
-  /*--- Setup sintial question ---*/
+  /*--- Allows user to select an answer from choices ---*/
+  $('#answer-choices').on('click', '.button', function() {
+    $('li > .button').removeAttr('id', 'selected-answer');
+    $(this).attr('id', 'selected-answer');
+  });
+
+  /*--- User submits answer. User answer is checked against correct answer. Correct or Incorrect is displayed ---*/
+  $('.right-container').on('click', '.submit', function() {
+    var answer = document.getElementById('selected-answer').value;
+    console.log(answer);
+    if (answer == questionBank[questionNumber].answer ) {
+      console.log('Correct');
+      numberCorrect++;
+      $("<h2>Correct!</h2>").insertBefore("#next-question");
+      $("#answer-box").addClass('correct');
+    } else {
+      console.log("Incorrect");
+      $("<h2>Incorrect!</h2>").insertBefore("#next-question");
+      $("#answer-box").addClass('incorrect');
+    }
+    showAnswerBox();
+  });
+
+  /*--- Quiz is reset to show new question. ---*/
+  $('#next-question').click(function() {
+    $('#question-box').empty();
+    $('#answer-choices').empty();
+    questionNumber++;
+    hideAnswerBox();
+    $('#question-status-box').empty();
+    setQuiz();
+  });
+
+  /*--- Setup quiz questions ---*/
   function setQuestion(questionInput) {
     $("<p>" + questionInput.question + "</p>").appendTo("#question-box");
   }
 
+  /*--- Setup answer choices ---*/
   function setAnswerChoices(answerInput) {
     for(var i = 0; i < answerInput.length; i++) {
       $("<li><input type=\"button\" class=\"button\" value=" + answerInput[i] + "></input></li>").appendTo('#answer-choices');
     }
   }
 
-  function setUserAnswer() {
-    $('li > .button').click(function() {
-      $('li > .button').removeClass('selected-answer');
-      $(this).addClass('selected-answer');
-    });
+  /*--- Display answer box on submit ---*/
+  function showAnswerBox() {
+    $('#answer-box').show();
+    $('#next-question').show();
   }
 
-  /*--- Run Quiz ---*/
-  function runQuiz() {
+  /*--- Hides answer box for next question. Clears correct and Incorrect classes ---*/
+  function hideAnswerBox() {
+    $('#answer-box').children('h2').remove();
+    $('#answer-box').removeClass('correct incorrect');
+    $('#answer-box').hide();
+    $('#next-question').hide();
+  }
+
+  /*--- Setup Quiz ---*/
+  function setQuiz() {
     var currentQuestion = questionBank[questionNumber];
-    // Set question
     setQuestion(currentQuestion);
-    // Set answer choices
     setAnswerChoices(currentQuestion.choices);
-    // Get answer user answer from answer choices
-    setUserAnswer();
-    // Check user answer vs actual answer
-    // if user answer = actual answer
-    // numberCorrect++
+    var questionNumberDisplay = questionNumber + 1;
+    $('#question-status-box').append('<p>Question ' + questionNumberDisplay + ' of 5</p>');
   }
 
-  runQuiz();
+  setQuiz();
+
 
   
 
